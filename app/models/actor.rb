c1fadef1f7b525d6e4c07029bfaff04962bf58bf
Anthony Pardo <pardo.anthony@gmail.com>
2020-03-07 17:33:01 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
+  has_many :characters 
+  has_many :shows, through: :characters
   
+  def full_name 
+    name = self.first_name + " " + self.last_name
+  end
+  
+  def list_roles 
+    arr = [] 
+    self.characters.each do |character|
+      str = "#{character.name} - #{character.show.name}"
+      arr << str 
+    end
+    arr 
+  end
 end
