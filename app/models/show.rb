737b2679fdfe9f3d5a3a0b5be60eaecfdb08ab14
Anthony Pardo <pardo.anthony@gmail.com>
2020-03-07 17:27:16 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,11 @@
 class Show < ActiveRecord::Base
   has_many :characters 
   has_many :actors, through: :characters
+  
+  def actors_list 
+    arr = [] 
+    self.actors.each do |actor|
+      arr << actor.
+    end
+  end
 end
